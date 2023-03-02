91643b38d45e1bab1aaa5cf6b72a9b6d157ef7d7
KuligKamil <kamil.marek.kulig@gmail.com>
2023-03-03 00:35:39 +0100

    🐼 chore: use properly props with v-text-field

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{
   text: string
   label: string
   loading: boolean
 }>()
 const emit = defineEmits<(event: 'update:text', text: string) => void>()
-const searchText = ref('')
 </script>
 
 <template>
   <v-text-field
-    v-model="searchText"
+    :model-value="text"
     :loading="loading"
     :label="label"
     prepend-inner-icon="mdi-magnify"
     single-line
-    hide-details
-    @update:model-value="emit('update:text', searchText)"
+    @update:model-value="emit('update:text', $event)"
   />
 </template>
